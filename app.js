e9807953c1d041755df24c78ef6447c47b656bc8
luanpontolio <luan.pontolio@gmail.com>
2018-10-05 00:28:49 -0300

    Some changes in webpack

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ import * as R from 'ramda';
 
 import sum, { sub, multiplicacao, div as dividir } from './utils';
 
+// import * as React from 'react';
+// import * as ReactDom from 'react-dom'
+
 const array1 = [1,1,1,1,2,3,3,3,4];
 const array2 = [2,3,5,6];
 
@@ -18,10 +21,10 @@ const array4 = R.uniq(array1);
 console.log(array3);
 console.log(array4);
 
-console.log(sum(3, 2));
+console.log(sum(15, 34));
 
 console.log(sub(3, 2));
 
-console.log(multiplicacao(3, 2));
+console.log(multiplicacao(10, 2));
 
 console.log(dividir(4, 2));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 var webpack = require('webpack');
 const path = require('path');
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+
+// Cria-se uma variavel da qual seu valor será definido de acordo com 
+// o que esta presente em process.env.NODE_EN, neste caso ela estará vazia
+// e será adicionado o valor 'production'.
+const nodeENV = process.env.NODE_ENV || 'production';
 
 
 // Toda a configuração do webpack é aqui dentro
 module.exports = {
+    devtool: 'source-map',
     entry: './app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -24,5 +31,23 @@ module.exports = {
             }
         ]
     }, 
+    plugins: [
+        new UglifyJSPlugin({
+          uglifyOptions: {
+            beautify: false,
+            ecma: 6,
+            compress: true,
+            comments: false,
+            mangle: false,
+          },
+          sourceMap: true
+        }),
+        // adiciona um novo plugin para definit o valor de 
+        // process.env, onde é criado uma variavel NODE_ENV para ser usada
+        // definindo qual o ambiente
+        new webpack.DefinePlugin({
+            'process.env': { NODE_ENV: JSON.stringify(nodeENV) }
+        })
+    ]
    
 }
